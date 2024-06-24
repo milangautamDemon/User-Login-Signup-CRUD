b141082c7ef6f490bf5cd7a55e21bf784950815d
Milan Gautam <milangautam071440304@gmail.com>
2024-06-24 23:56:42 +0545

    refactor: router

--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,13 @@
 import express from "express";
 import loginController from "../controllers/login.js";
 import registerController from "../controllers/register.js";
+import updateController from "../controllers/update.js";
 
 const router = express.Router();
 
 router.use("/auth", loginController);
 router.use("/auth", registerController);
+router.use("/auth", updateController);
 
 export { router };
 export default router;
