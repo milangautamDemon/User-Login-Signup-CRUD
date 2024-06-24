69aac55d4c7a7144c9a72a66118290d7d19aa9d0
Milan Gautam <milangautam071440304@gmail.com>
2024-06-24 16:40:06 +0545

    Refactor: user register controller

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,16 +1,42 @@
 import express from "express";
+import User from "../model/user.js";
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  //get user data from data for registration
   const { userName, password } = req.body;
+
+  //check if username already exists
+  const isExists = await User.findOne({ userName: userName });
+
+  //if exists then send errors message
+  if (isExists) {
+    return res.status(400).send({
+      success: false,
+      err_msg: "USERNAME ALREADY EXISTS !!!",
+      msg: "UserName is already exists!!!",
+    });
+  }
+
+  //generate Salt
+  const genSalt = 10;
+
+  //create hash password
+  const hashedPassword = await bcrypt.hash(password, genSalt);
+
+  //create new user
+  const newUser = new User({
+    userName,
+    password: hashedPassword,
+  });
+
+  //register & save new user
+  await newUser.save();
+
   res.status(200).send({
     success: true,
-    msg: "User Register Successfully!",
-    data: {
-      userName,
-      password,
-    },
+    msg: "User Registered Successfully!",
   });
 });
 
