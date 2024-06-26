d302490055d4d9e83781833a082905d183843966
Milan Gautam <milangautam071440304@gmail.com>
2024-06-26 12:04:00 +0545

    refactor: get user data api

--- client/script.js
+++ client/script.js
@@ -1,36 +1,61 @@
-import axios from "axios";
+const apiUrl = "http://localhost:3000/api/auth";
 
 $(document).ready(function () {
-  const apiUrl = "http://localhost:3000/api/auth";
-
   // Register user
-  $("#registrationForm").submit(function (e) {
+  $("#registrationForm").submit(async function (e) {
     e.preventDefault();
     const userName = $("#userName").val();
     const password = $("#password").val();
 
-    axios
-      .post(`${apiUrl}/register`, { userName, password })
-      .then((response) => {
-        alert("User registered successfully");
-        $("#registrationForm")[0].reset();
-        window.location.href = "login.html";
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error registering the user!", error);
-        if (error.response) {
-          console.error("Response status:", error.response.status);
-          console.error("Response data:", error.response.data);
-          alert(`Failed to register user: ${error.response.data.msg}`);
-        } else if (error.request) {
-          console.error(
-            "Request made but no response received:",
-            error.request
-          );
-        } else {
-          console.error("Error setting up the request:", error.message);
-        }
+    try {
+      const response = await axios.post(`${apiUrl}/register`, {
+        userName,
+        password,
       });
+      alert("User registered successfully");
+      $("#registrationForm")[0].reset();
+      loadUsers();
+      window.location.href = "login.html";
+      console.log(response.data.data);
+    } catch (error) {
+      console.error("There was an error registering the user!", error);
+      if (error.response) {
+        console.error("Response status:", error.response.status);
+        console.error("Response data:", error.response.data);
+        alert(`Failed to register user: ${error.response.data.msg}`);
+      } else if (error.request) {
+        console.error("Request made but no response received:", error.request);
+      } else {
+        console.error("Error setting up the request:", error.message);
+      }
+    }
   });
+
+  //load function
+  async function loadUsers() {
+    try {
+      const response = await axios.get(`${apiUrl}/users`);
+      console.log(response);
+      const users = response.data.data;
+      //check whether the users data receive or not
+      console.log(users);
+      //get table body element by id
+      const tableBody = $("#user-table tbody");
+      tableBody.empty(); // Clear existing table data
+
+      users.forEach((user) => {
+        const tableRow = `<tr data-username=${user.userName}>
+          <td>${user.id}</td>
+          <td>${user.userName}</td>
+        </tr>`;
+        tableBody.append(tableRow);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //Initial load users when the document is ready
+  loadUsers();
+  console.log(loadUsers());
 });
